d250fca6aad0a6fa54cf1d6e5c9739b0f22d94af
DevKeyLPlus <luciosantospro@gmail.com>
2020-05-06 21:59:14 -0300

    Adicionando exercicio 3 / Upgrade arquivo ReadME

--- ex03.php
+++ ex03.php
@@ -5,5 +5,23 @@
 </head>
 <body>
 
+	<form method="post" action="">
+		Digite um numero <input type="text" name="num1">
+		<button name="btCalcular">Calcular</button>
+	</form>
+
+	<?php 
+
+		if (isset($_POST['btCalcular'])) {
+			$num1 = $_POST['num1'];
+
+			$calc = ($num1 /100) * 15;
+
+			echo "15% de $num1 é $calc";
+		}
+
+
+	 ?>
+
 </body>
 </html>
